c201270c6025428d7fbe3a16474383eb8c734fcf
ToNaza <misterket37@gmail.com>
2024-04-18 01:29:59 +0300

    new update - teacher

---
+++ Професії/ProfJS.js
@@ -0,0 +1,7 @@
+document.getElementById("openSD").addEventListener("click", function() {
+document.getElementById("modSD").classList.add("open")
+})
+
+document.getElementById("closeSD").addEventListener("click", function() {
+document.getElementById("modSD").classList.remove("open")
+})
