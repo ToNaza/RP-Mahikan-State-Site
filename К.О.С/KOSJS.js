2d3511934498b114a0fd48098faa4447c9506ed9
ToNaza <misterket37@gmail.com>
2024-04-18 18:40:05 +0300

    uppdate - K.O.S.

---
+++ К.О.С/KOSJS.js
@@ -0,0 +1,16 @@
+document.getElementById("openSYD").addEventListener("click", function() {
+document.getElementById("modSYD").classList.add("open")
+})
+    
+document.getElementById("closeSYD").addEventListener("click", function() {
+document.getElementById("modSYD").classList.remove("open")
+})
+
+
+    document.getElementById("openNav").addEventListener("click", function() {
+    document.getElementById("modNav").classList.add("open")
+    })
+        
+    document.getElementById("closeNav").addEventListener("click", function() {
+    document.getElementById("modNav").classList.remove("open")
+    })
