eb613c00876227137ab84de2fd0da3104ab3982a
ToNaza <misterket37@gmail.com>
2024-04-18 14:58:50 +0300

    updade teacher

--- Професії/ProfJS.js
+++ Професії/ProfJS.js
@@ -5,3 +5,21 @@ document.getElementById("modSD").classList.add("open")
 document.getElementById("closeSD").addEventListener("click", function() {
 document.getElementById("modSD").classList.remove("open")
 })
+
+
+    document.getElementById("openAoSB").addEventListener("click", function() {
+    document.getElementById("modAoSB").classList.add("open")
+    })
+    
+    document.getElementById("closeAoSB").addEventListener("click", function() {
+    document.getElementById("modAoSB").classList.remove("open")
+    })
+
+
+        document.getElementById("openGSB").addEventListener("click", function() {
+        document.getElementById("modGSB").classList.add("open")
+        })
+        
+        document.getElementById("closeGSB").addEventListener("click", function() {
+        document.getElementById("modGSB").classList.remove("open")
+        })
