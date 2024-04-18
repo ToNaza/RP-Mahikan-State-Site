db03d23fbad682598e7c19271f256f9f8f73427e
ToNaza <misterket37@gmail.com>
2024-04-18 23:04:22 +0300

    test fix3

--- Реєстрація/RegJS.js
+++ Реєстрація/RegJS.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('myForm');
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz_Ao-06saTHRXFzFqBsfKYh6kr-QhzaZNOc5De4Ocegds6CS3w4VoeiM8g373B4_ml/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzIPwR8yhbIwVaDecsU66nwZ_dAjNxHj1TF0TVjYsAislyNsc2L4iU5jyx_p1QhRsI/exec';
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -20,6 +20,8 @@ form.addEventListener('submit', e => {
     })
     .catch(error => console.error('Error sending message:', error));
 });
+
+
 
 
 
