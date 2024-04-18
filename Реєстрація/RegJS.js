100ee4c8c25ae7fb3a748065c2b83818719d382e
ToNaza <misterket37@gmail.com>
2024-04-18 22:57:39 +0300

    test fix2

--- Реєстрація/RegJS.js
+++ Реєстрація/RegJS.js
@@ -1,19 +1,25 @@
+const form = document.getElementById('myForm');
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz_Ao-06saTHRXFzFqBsfKYh6kr-QhzaZNOc5De4Ocegds6CS3w4VoeiM8g373B4_ml/exec';
 
-fetch(proxyUrl + scriptURL, { 
-  method: 'POST', 
-  body: new FormData(form)
-})
-.then(response => {
-  if (response.ok) {
-    console.log('Message sent successfully!');
-    form.reset();
-  } else {
-    console.error('Failed to send message.');
-  }
-})
-.catch(error => console.error('Error sending message:', error));
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    fetch(proxyUrl + scriptURL, { 
+        method: 'POST', 
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Message sent successfully!');
+            form.reset();
+        } else {
+            console.error('Failed to send message.');
+        }
+    })
+    .catch(error => console.error('Error sending message:', error));
+});
 
 
 
