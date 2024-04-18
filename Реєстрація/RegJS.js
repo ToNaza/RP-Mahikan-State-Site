0514a794929f62ca69e678ff10ef4c4e80f586b6
ToNaza <misterket37@gmail.com>
2024-04-18 22:54:23 +0300

    test fix1

--- Реєстрація/RegJS.js
+++ Реєстрація/RegJS.js
@@ -1,25 +1,20 @@
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz_Ao-06saTHRXFzFqBsfKYh6kr-QhzaZNOc5De4Ocegds6CS3w4VoeiM8g373B4_ml/exec';
-const form = document.forms['submit-to-google-sheet'];
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => {
-      if (response.ok) {
-        console.log('Message sent successfully!');
-        // Очистити форму після успішної відправки
-        form.reset();
-      } else {
-        console.error('Failed to send message.');
-      }
-    })
-    .catch(error => console.error('Error sending message:', error));
-});
+fetch(proxyUrl + scriptURL, { 
+  method: 'POST', 
+  body: new FormData(form)
+})
+.then(response => {
+  if (response.ok) {
+    console.log('Message sent successfully!');
+    form.reset();
+  } else {
+    console.error('Failed to send message.');
+  }
+})
+.catch(error => console.error('Error sending message:', error));
 
-fetch('https://example.com/api/data', { mode: 'no-cors' })
-  .then(response => {
-    console.log('Request sent successfully');
-  })
-  .catch(error => console.error('Error sending request:', error));
+
 
   
