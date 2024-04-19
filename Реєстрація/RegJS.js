7a2e13eff5db9b202bd74a328bea2b49daeeaa9d
ToNaza <misterket37@gmail.com>
2024-04-19 19:55:09 +0300

    update Arial

--- Реєстрація/RegJS.js
+++ Реєстрація/RegJS.js
@@ -23,3 +23,58 @@ document.getElementById("open").addEventListener("click", function() {
             document.getElementById('passwordInput').value = '';
         }
     }
+
+
+    function checkPassword() {
+        const passwordInput = document.getElementById('passwordInput').value;
+        const errorMessageModal = document.getElementById('errorMessageModal');
+        const errorMessage = document.getElementById('errorMessage');
+    
+        // Перевірка правильності пароля (приклад: "123456")
+        if (passwordInput === '123456') {
+            // Показ картинки на 2 секунди
+            showImageForTwoSeconds();
+    
+        } else {
+            // Показ повідомлення про помилку
+            errorMessage.textContent = 'Пароль введено неправильно';
+            errorMessageModal.style.display = 'block';
+        }
+    }
+    
+    function checkPassword() {
+        const passwordInput = document.getElementById('passwordInput').value;
+        const errorMessageModal = document.getElementById('errorMessageModal');
+        const errorMessage = document.getElementById('errorMessage');
+    
+        // Перевірка правильності пароля (приклад: "123456")
+        if (passwordInput === 'Argemia.pls1724K13B1M84im3') {
+            // Показ картинки на 2 секунди
+            showImageForTwoSeconds();
+    
+        } else {
+            // Показ повідомлення про помилку
+            errorMessage.textContent = 'Пароль введено неправильно';
+            errorMessageModal.style.display = 'block';
+        }
+    }
+    
+    function showImageForTwoSeconds() {
+        const imageElement = document.createElement('img');
+        imageElement.src = 'https://img2.joyreactor.cc/pics/post/Voices-Of-The-Void-%D0%98%D0%B3%D1%80%D1%8B-Arirals-8342149.png'; // Замініть шлях до зображення
+        document.body.appendChild(imageElement);
+    
+        imageElement.style.width = '200px'; // Задаємо ширину зображення в 200px
+        
+        document.body.appendChild(imageElement);
+
+        // Затримка показу картинки на 2 секунди перед видаленням
+        setTimeout(() => {
+            document.body.removeChild(imageElement);
+        }, 2000);
+    }
+    
+    function hideErrorMessage() {
+        const errorMessageModal = document.getElementById('errorMessageModal');
+        errorMessageModal.style.display = 'none';
+    }
