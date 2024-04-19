d4bacb07dce72583b54e31d6be3a40fad013864d
ToNaza <misterket37@gmail.com>
2024-04-20 00:20:29 +0300

    new update

--- Реєстрація/RegJS.js
+++ Реєстрація/RegJS.js
@@ -21,6 +21,12 @@ function checkPassword() {
     } else if (passwordInput === 'Argemia.pls1724K13B1M84im3') {
         // Показ картинки на 2 секунди
         showImageForTwoSeconds('https://img2.joyreactor.cc/pics/post/Voices-Of-The-Void-%D0%98%D0%B3%D1%80%D1%8B-Arirals-8342149.png', 200);
+    } else if (passwordInput === 'Admit226317') {
+        // Відкриття посилання 1
+        window.open('https://t.me/+BCo7JhGt_L05YWUy');
+    } else if (passwordInput === 'KOS843211') {
+        // Відкриття посилання 2
+        window.open('https://t.me/+li3tkW0GhFwzOTRi');
     } else {
         // Показ повідомлення про помилку
         errorMessage.textContent = 'Неправильний пароль, спробуйте ще раз';
