1f45bea7e161273795c8151a1507f706f3835627
ToNaza <misterket37@gmail.com>
2024-04-20 17:06:02 +0300

    test

--- Реєстрація/RegJS.js
+++ Реєстрація/RegJS.js
@@ -8,3 +8,28 @@ document.getElementById("close").addEventListener("click", function() {
 
 
     
+function submitForm() {
+    const form = document.getElementById('MyForm');
+    const formData = new FormData(form);
+
+    // Отримати дані форми у форматі JSON
+    const jsonData = {};
+    formData.forEach((value, key) => {
+        jsonData[key] = value;
+    });
+
+    // Відправити дані на сервер за допомогою AJAX-запиту
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '../Професії/GRinfoHTML.html');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            // Отримано успішну відповідь від сервера
+            console.log('Дані успішно відправлено');
+            // Тут ви можете додати додаткові дії після успішної обробки даних
+        } else {
+            console.error('Сталася помилка під час відправлення даних');
+        }
+    };
+    xhr.send(JSON.stringify(jsonData));
+}
