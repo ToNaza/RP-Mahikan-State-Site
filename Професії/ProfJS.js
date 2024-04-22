27dcae65779ae89203e7f052c34ae7d6ec5113d8
ToNaza <misterket37@gmail.com>
2024-04-22 19:06:25 +0300

    new update

--- Професії/ProfJS.js
+++ Професії/ProfJS.js
@@ -55,3 +55,55 @@ document.getElementById("modSD").classList.remove("open")
                 }, 1000);
             });
         });
+
+
+
+            document.getElementById("open_tSD").addEventListener("click", function() {
+                document.getElementById("teachingSD").classList.add("open")
+                document.getElementById("modSD").classList.remove("open")
+                })
+               
+
+                document.getElementById("closetSD").addEventListener("click", function() {
+                document.getElementById("teachingSD").classList.remove("open")
+                })
+
+
+                /*Відправка в ТГ СД*/ 
+
+                function sendMessage() {
+                    const messageInput = document.getElementById('messageInput');
+                    const messageText = messageInput.value.trim(); // Получаем текст сообщения из поля ввода
+                
+                    if (messageText === '') {
+                        alert( Громадянин вибирає прохід навчання в категорію Служби Допомоги);
+                        return;
+                    }
+                
+                    const botToken = 6341044419:AAGoMhCmTOlbQMApMfcZzOgNfdAS36d0XP4;
+                    const chatId = https://t.me/ProfApplication_Bot;
+                
+                    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+                    const data = {
+                        chat_id: chatId,
+                        text: messageText
+                    };
+                
+                    const xhr = new XMLHttpRequest();
+                    xhr.open('POST', url, true);
+                    xhr.setRequestHeader('Content-Type', 'application/json');
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState === XMLHttpRequest.DONE) {
+                            if (xhr.status === 200) {
+                                alert('Сообщение успешно отправлено в Telegram');
+                                messageInput.value = ''; // Очистить поле ввода после отправки
+                            } else {
+                                alert('Произошла ошибка при отправке сообщения');
+                            }
+                        }
+                    };
+                    xhr.send(JSON.stringify(data));
+                }
+
+                /*Закінчення відправки*/
+                /*Початок відправки Ао СБ*/
