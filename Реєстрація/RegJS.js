9f92da9f3af0a4e6acb1cb3e3daef0f3e47b3f14
ToNaza <misterket37@gmail.com>
2024-04-23 21:58:06 +0300

    test fixed

--- Реєстрація/RegJS.js
+++ Реєстрація/RegJS.js
@@ -9,17 +9,17 @@ document.getElementById("close").addEventListener("click", function() {
 
     
 const TOKEN = "6341044419:AAGoMhCmTOlbQMApMfcZzOgNfdAS36d0XP4";
-const CHAT_ID = "-1002007292915";
+const CHAT_ID = " -1002007292915";
 const URI_API = `https://api.telegram.org/bot${ TOKEN }/senMassege`;
 
 document.getElementById('tg').addEventListener('sumbit', function(e) {
     e.preventDefault();
 
     let message = `<b>Новий Громадянин!</b> \n.`;
-    message += `<b>Ім'я-</b> ${this.Name.value}`;
-    message += `<b>Фамілія-</b> ${this.Surname.value}`;
-    message += `<b>Username-</b> ${this.Username.value}`;
-    message += `<b>Дата входу-</b> ${this.entryDate.value}`;
+    message += `<b>Ім'я-</b> ${this.Name.value} \n`;
+    message += `<b>Фамілія-</b> ${this.Surname.value} \n`;
+    message += `<b>Username-</b> ${this.Username.value} \n`;
+    message += `<b>Дата входу-</b> ${this.entryDate.value} \n`;
     message += `<b>ID-</b> ${Date.now()}`;
 
    axios.post(URI_API, {
