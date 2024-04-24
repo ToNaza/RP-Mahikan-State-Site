7054da3c924b31071e85163cdaa28d819bc0c200
ToNaza <misterket37@gmail.com>
2024-04-24 22:20:15 +0300

    vers

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-var versionText = 'Preparation for release 101';
+var versionText = 'Preparation for release 102';
 
 function setVersionText() {
     // Отримати елемент <p> для відображення версії
