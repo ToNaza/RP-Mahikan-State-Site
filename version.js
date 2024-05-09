6511438a22d5118f727ac8e944b531164a319e92
ToNaza <misterket37@gmail.com>
2024-05-09 15:51:42 +0300

    1.1/4

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-var versionText = '1.1/3';
+var versionText = '1.1/4';
 
 function setVersionText() {
     // Отримати елемент <p> для відображення версії
---
+++ Новини/NewsJS.js
@@ -0,0 +1,7 @@
+document.getElementById("openpost3").addEventListener("click", function() {
+    document.getElementById("newsopenpost3").classList.add("open")
+    })
+    
+    document.getElementById("closepost3").addEventListener("click", function() {
+    document.getElementById("newsopenpost3").classList.remove("open")
+    })
